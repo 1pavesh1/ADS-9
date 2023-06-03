5c270f72ddca7a8b016b7cce8f97fd980b7eec7b
1pavesh1 <125039843+1pavesh1@users.noreply.github.com>
2023-06-03 22:20:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,46 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+std::ifstream file(filename);
+        std::string copy;
+        std::string word;
+        std::string clear;
+        while (!file.eof())
+        {
+            std::getline(file, copy);
+            for (size_t i = 0; i < copy.size(); ++i) {
+                if (copy[i] >= 65 && copy[i] <= 90 || copy[i] >= 97 && copy[i] <= 122) {
+                    if (copy[i] >= 65 && copy[i] <= 90) {
+                        word += copy[i] + 32;
+                    } else {
+                        word += copy[i];
+                    }
+                }
+                if (copy[i + 1] == ' ' && word.size() == 1 || word.size() == 1 && i + 1 == copy.size()) {
+                    word = clear;
+                }
+                if (copy[i] == ' ' && word != clear) {
+                    word = clear;
+                } else if (copy[i + 1] == ' ' && word != clear || i + 1 == copy.size() && word != clear || copy[i] == 39 && word != clear) {
+                    if (root == nullptr) {
+                        root = new node;
+                        root->word = word;
+                        root->count = 1;
+                        root->left = root->right = nullptr;
+                    } else if (root->word > word) {
+                        root->left = addNode(root->left, word);
+                    } else if (root->word < word) {
+                        root->right = addNode(root->right, word);
+                    } else if (root->word == word) {
+                        root->count += 1;
+                    }
+                    if (i + 1 == copy.size()) {
+                        word = clear;
+                        break;
+                    }
+                }
+            }
+        }
+        file.close();
+        return word;
 }
