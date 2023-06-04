48175fb08955f77e3d0d52eedb4b81d45f352c81
1pavesh1 <125039843+1pavesh1@users.noreply.github.com>
2023-06-04 23:08:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,31 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <locale>
+#include <cstdlib>
+#include <algorithm> 
+template <typename T>
+class BST {
 
-BST<std::string> makeTree(const char* filename) {
-std::ifstream file(filename);
+private:
+
+    struct node {
+        T word;
+        int count;
+        node *left, *right;
+    };
+    node *root;
+    int Depth;
+
+public:
+
+    BST() {
+        root = nullptr;
+        Depth = 0;
+    }
+    std::string makeTree(const char* filename) {
+        std::ifstream file(filename);
         std::string copy;
         std::string word;
         std::string clear;
@@ -47,4 +66,55 @@ std::ifstream file(filename);
         }
         file.close();
         return word;
-}
+    }
+    node* addNode(node *root, const T& word) {
+        if (root == nullptr) {
+            root = new node;
+            root->word = word;
+            root->count = 1;
+            root->left = root->right = nullptr;
+        } else if (root->word > word) {
+            root->left = addNode(root->left, word);
+        } else if (root->word < word) {
+            root->right = addNode(root->right, word);
+        } else if (root->word == word) {
+            root->count += 1;
+        }
+        return root;
+    }
+    int search(std::string word) {
+        if (root == nullptr) {
+            return 0;
+        } else {
+            if (root->word == word) {
+                return root->count;
+            } else {
+                while (root->word != word) {
+                    if (root->word > word) {
+                        root = root->left;
+                    } else if (root->word < word) {
+                        root = root->right;
+                    }
+                }
+                return root->count;
+            }
+        }
+    }
+    int height(node* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        return 1 + std::max(height(root->left), height(root->right));
+    }
+    int depth() {
+        if (root == nullptr) {
+            return 0;
+        } else {
+            Depth += 1 + std::max(height(root->left), height(root->right));
+            return Depth - 2;
+        }
+    }
+    ~BST() {
+        delete root;
+    }
+};
